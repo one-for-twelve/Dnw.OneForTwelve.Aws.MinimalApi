a26411d4e5ab26198e2eda5bfd299fd1e75ff3ca
jbijlsma <jeroen_work@yahoo.com>
2023-01-26 21:56:15 +0800

    Make auth solution that works with AOT

--- Api/Handlers.cs
+++ Api/Handlers.cs
@@ -1,11 +1,15 @@
+using System.Collections.Concurrent;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using Dnw.OneForTwelve.Core.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Protocols;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
+using Shared;
 using Shared.Clients;
 
 namespace Api;
@@ -14,6 +18,7 @@ internal static class Handlers
 {
     internal static ILogger? Logger;
     internal static IGameClient? GameClient;
+    private static readonly ConcurrentDictionary<string, SecurityKey> SecurityKeysByKid = new();
 
     public static async Task Default(HttpContext context)
     {
@@ -32,48 +37,83 @@ internal static class Handlers
 
         const string authority = "https://securetoken.google.com/one-for-twelve-32778";
         
-        var validIssuers = new List<string> { authority };
-
-        var configManager = new ConfigurationManager<OpenIdConnectConfiguration>($"{validIssuers.Last()}/.well-known/openid-configuration", new OpenIdConnectConfigurationRetriever());
-
-        var oidcConfig = configManager.GetConfigurationAsync().Result;
-
-        // var token = authHeader.Replace("Bearer ", "", StringComparison.OrdinalIgnoreCase);
-        // try
-        // {
-        //     var tokenValidationParams = new TokenValidationParameters
-        //     {
-        //         ValidateIssuer = true,
-        //         ValidIssuer = authority,
-        //         
-        //         ValidateAudience = true,
-        //         ValidAudience = "one-for-twelve-32778",
-        //         
-        //         ValidateIssuerSigningKey = true,
-        //         IssuerSigningKeys = oidcConfig.SigningKeys,
-        //         
-        //         ValidateLifetime = true
-        //     };
-        //     var jsonWebTokenHandler = new JsonWebTokenHandler();
-        //     var tokenValidationResult = jsonWebTokenHandler.ValidateToken(token, tokenValidationParams);
-        //     if (!tokenValidationResult.IsValid)
-        //     {
-        //         // Handle each exception which tokenValidationResult can contain as appropriate for your service
-        //         // Your service might need to respond with a http response instead of an exception.
-        //         if (tokenValidationResult.Exception != null)
-        //             throw tokenValidationResult.Exception;
+        // var validIssuers = new List<string> { authority };
         //
-        //         throw new ApplicationException("Token Validation Failed");
-        //     }
-        //     
-        //     var uid = tokenValidationResult.ClaimsIdentity.Name;
-        //     Logger?.LogInformation("{uid}", uid);
-        // }
-        // catch (Exception ex)
-        // {
-        //     Logger?.LogError(ex, "{message}", ex.Message);
-        //     throw;
-        // }
+        // var configManager = new ConfigurationManager<OpenIdConnectConfiguration>($"{validIssuers.Last()}/.well-known/openid-configuration", new OpenIdConnectConfigurationRetriever());
+        //
+        // var oidcConfig = configManager.GetConfigurationAsync().Result;
+        
+        var token = authHeader.Replace("Bearer ", "", StringComparison.OrdinalIgnoreCase);
+        
+        var headerBase64 = token.Split('.')[0];
+        var headerJson = Base64Decode(headerBase64);
+        var header = JsonSerializer.Deserialize<JsonWebHeader>(headerJson);
+        var kid = header!.Kid;
+
+        if (!SecurityKeysByKid.ContainsKey(kid))
+        {
+            const string configAddress = $"{authority}/.well-known/openid-configuration";
+            var retriever = new HttpDocumentRetriever();
+            var ct = CancellationToken.None;
+            var configAsString = await retriever.GetDocumentAsync(configAddress, ct);
+        
+            var oidcConfig = JsonSerializer.Deserialize<OidcConfigCustom>(configAsString)!;
+            if (!string.IsNullOrEmpty(oidcConfig.JwksUri))
+            {
+                var keys = await retriever.GetDocumentAsync(oidcConfig.JwksUri, ct).ConfigureAwait(false);
+            
+                var keySet = JsonSerializer.Deserialize<JsonWebKeySetCustom>(keys)!;
+
+                foreach (var key in keySet.Keys)
+                {
+                    if (key.Kid != kid) continue;
+                    
+                    var rsaParams = new RSAParameters
+                    {
+                        Modulus = Base64UrlEncoder.DecodeBytes(key.N),
+                        Exponent = Base64UrlEncoder.DecodeBytes(key.E)
+                    };
+                    SecurityKeysByKid.TryAdd(kid, new RsaSecurityKey(rsaParams));
+                }
+            }
+        }
+        
+
+        try
+        {
+            var tokenValidationParams = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = authority,
+                
+                ValidateAudience = true,
+                ValidAudience = "one-for-twelve-32778",
+                
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKeys = new [] { SecurityKeysByKid[kid] },
+                
+                ValidateLifetime = true
+            };
+            var jsonWebTokenHandler = new JsonWebTokenHandler();
+            var tokenValidationResult = jsonWebTokenHandler.ValidateToken(token, tokenValidationParams);
+            if (!tokenValidationResult.IsValid)
+            {
+                // Handle each exception which tokenValidationResult can contain as appropriate for your service
+                // Your service might need to respond with a http response instead of an exception.
+                if (tokenValidationResult.Exception != null)
+                    throw tokenValidationResult.Exception;
+        
+                throw new ApplicationException("Token Validation Failed");
+            }
+            
+            var uid = tokenValidationResult.ClaimsIdentity.Name;
+            Logger?.LogInformation("{uid}", uid);
+        }
+        catch (Exception ex)
+        {
+            Logger?.LogError(ex, "{message}", ex.Message);
+            throw;
+        }
 
         var languageAsString = context.Request.RouteValues["language"]?.ToString()!;
         var strategyAsString = context.Request.RouteValues["questionSelectionStrategy"]?.ToString()!;
@@ -86,5 +126,17 @@ internal static class Handlers
         Logger?.LogInformation($"Game started: {game.Word}");
         
         await context.WriteResponse(HttpStatusCode.OK, game);
+    }
+    
+    private static string Base64Decode(string token)
+    {
+        token = token.Replace('_', '/').Replace('-', '+');
+        switch (token.Length % 4)
+        {
+            case 2: token += "=="; break;
+            case 3: token += "="; break;
+        }       
+        var decoded = Convert.FromBase64String(token);
+        return Encoding.Default.GetString(decoded);
     }
 }
--- Shared/ApiSerializerContext.cs
+++ Shared/ApiSerializerContext.cs
@@ -8,6 +8,10 @@ namespace Shared;
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
 [JsonSerializable(typeof(Game))]
+[JsonSerializable(typeof(OidcConfigCustom))]
+[JsonSerializable(typeof(JsonWebKeySetCustom))]
+[JsonSerializable(typeof(JsonWebKeyCustom))]
+[JsonSerializable(typeof(JsonWebHeader))]
 public partial class ApiSerializerContext : JsonSerializerContext
 {
 }
